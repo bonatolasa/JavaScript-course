47ac9b8c899015519fe9465f5feee83de14eac67
BonaTolasa <146835715+bonatolasa@users.noreply.github.com>
2025-02-26 17:10:09 +0300

    first Guess Game commit

--- 03-Guess-my-number/script.js
+++ 03-Guess-my-number/script.js
@@ -1,11 +1,98 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'correct number';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'correct number';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //When there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔ No Number';
+    displayMessage('⛔ No Number');
+  }
+
+  //When players win
+  else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🏆 Correct Number';
+    displayMessage('🏆 Correct Number');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+
+  //When guess is to wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈 Too High' : '📉 Too Low';
+      displayMessage(guess > secretNumber ? '📈 Too High' : '📉 Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = '✨You Lost The Game';
+      displayMessage('✨You Lost The Game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  //When guess is to high
+  // else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too High';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '✨You Lost The Game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+
+  //When guess is to low
+  // else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Too Low';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '✨You Lost The Game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
+
+//my challenge
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
