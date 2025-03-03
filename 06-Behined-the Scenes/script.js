1483a49acd20bb385af1613bd7f0d77dcf0666c5
BonaTolasa <146835715+bonatolasa@users.noreply.github.com>
2025-03-03 17:54:22 +0300

    updated js commit

--- 06-Behined-the Scenes/script.js
+++ 06-Behined-the Scenes/script.js
@@ -51,7 +51,7 @@ const bonah = {
   year: 2011,
   calcAge: function () {
     console.log(this);
-    console.log(2037 - this.year);
+    // console.log(2037 - this.year);
   },
 };
 bonah.calcAge();
@@ -65,3 +65,82 @@ matilda.calcAge();
 
 const f = bonah.calcAge;
 f();
+
+// var FirstName = ' Matillena';
+
+const bona = {
+  firstName: 'boni',
+  year1: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year1);
+
+    //Solution 1
+    // const self = this; //self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year1 >= 1981 && self.year1 <= 1996);
+    // };
+
+    //Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year1 >= 1981 && this.year1 <= 1996);
+    };
+    isMillenial();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.FirstName}`);
+  },
+};
+bona.greet();
+bona.calcAge();
+
+// argument Keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 6, 7);
+
+// var addArrow = (a, b) => {
+//   console.log(arguments);
+//   return a + b;
+// };
+// addArrow(2, 5, 8);
+
+//Primitive types
+let LastName = '  Tolasa';
+let oldLastName = LastName;
+LastName = 'Gutama';
+console.log(LastName, oldLastName);
+
+//refrencial type
+const jessica = {
+  firstName: 'jessica',
+  LastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.LastName = 'Davie';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+//copying object
+const jessica2 = {
+  firstName: 'jessica',
+  LastName: 'Williams',
+  age: 27,
+  family: ['Bontu', 'latera'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.LastName = 'Davis';
+
+jessicaCopy.family.push('marry');
+jessicaCopy.family.push('john');
+
+console.log('Before marriage: ', jessica2);
+console.log('After marriage: ', jessicaCopy);
