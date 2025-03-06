b9755af58c6cc387d5aedaadad861c57a41e6a2b
BonaTolasa <146835715+bonatolasa@users.noreply.github.com>
2025-03-06 19:34:36 +0300

    about game play first challenge

---
+++ 07-Data-Structure-Operators/challenges.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1.
+const [player1, player2] = game.players;
+console.log(player1, player2);
+
+// 2 .
+const [gk, ...fieldPlayers] = player1;
+console.log(gk, fieldPlayers);
+
+// 3.
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+// 4 .
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Periscic'];
