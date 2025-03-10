6e1f82c6862f0b8a06bc43324c4887852fa24396
BonaTolasa <146835715+bonatolasa@users.noreply.github.com>
2025-03-10 08:07:32 +0300

    update second challenge commit

--- 07-Data-Structure-Operators/challenges.js
+++ 07-Data-Structure-Operators/challenges.js
@@ -60,7 +60,7 @@ console.log(average);
 
 // 3.
 for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw' : 'victory ${game[team]}';
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr} ${odd}`);
 }
 
