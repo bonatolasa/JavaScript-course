ff2c09571726cd166d644c459cd8bb510f275631
BonaTolasa <146835715+bonatolasa@users.noreply.github.com>
2025-03-12 08:58:16 +0300

    working with string part2

--- 07-Data-Structure-Operators/script.js
+++ 07-Data-Structure-Operators/script.js
@@ -68,6 +68,70 @@ const restaurant = {
   },
 };
 
+/////////////////////////////
+//working with string part2
+const airline = 'TAP Air Ethiopia';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+//Fix capitalization in name
+const passenger = 'bOnAh'; //Bonah
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passenger.toLowerCase().slice(1);
+console.log(passengerCorrect);
+
+// Comparing emails
+const email = 'hello@boni.io';
+const loginEmail = ' hello@boni.io \n';
+
+const lowerEmail = loginEmail.toLowerCase();
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+//replacing
+const priceGB = '266.34£';
+// const priceGB = '\u00A3266.34';
+// console.log(priceGB);
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All Passengers come to boarding door 23.Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+//Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+
+  //Practice exercise
+  const checkBaggage = function (items) {
+    const baggage = items.toLowerCase();
+    if (baggage.includes('knife') || baggage.includes('gun')) {
+      console.log('You are not allowed on board');
+    } else {
+      console.log('You Welcome aboard!');
+    }
+  };
+  checkBaggage('I have a laptop, some Food and a pocket Knife');
+  checkBaggage('Socks and camera');
+  checkBaggage('Got some snacks and a gun for projection');
+}
+
+/*
 ////////////////////////////
 //Working with string-part1
 const airline = 'TAP Air Ethiopia';
@@ -109,6 +173,7 @@ console.log(new String('Bonahi'));
 console.log(typeof new String('Bonahi'));
 
 console.log(typeof new String('Bonahi').slice(-1));
+*/
 
 /*
 ////////////////////////////////
