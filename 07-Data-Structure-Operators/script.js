2d8f37e75c3229c1e4e578270c7dbf6e62643137
BonaTolasa <146835715+bonatolasa@users.noreply.github.com>
2025-03-12 08:59:19 +0300

    Split and Join commit

--- 07-Data-Structure-Operators/script.js
+++ 07-Data-Structure-Operators/script.js
@@ -68,6 +68,57 @@ const restaurant = {
   },
 };
 
+///////////////////////////
+// Split and Join
+console.log('a+very+nice+string'.split('+'));
+console.log('Bona Tolasa'.split(' '));
+
+const [firstName, lastName] = 'Bona Tolasa'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+capitalizeName('jessica ann smith davis');
+capitalizeName('bona tolasa');
+
+// Padding
+const message = 'Go to gate 23';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Bona'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  // console.log(str);
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(38690357));
+console.log(maskCreditCard(34456548690357));
+console.log(maskCreditCard('34456548690357'));
+
+//Repeat
+const message2 = 'Bad weather... All Departues Delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`Ther are ${n} planes in line ${'🛬'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
 /////////////////////////////
 //working with string part2
 const airline = 'TAP Air Ethiopia';
@@ -130,6 +181,7 @@ if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
   checkBaggage('Socks and camera');
   checkBaggage('Got some snacks and a gun for projection');
 }
+*/
 
 /*
 ////////////////////////////
