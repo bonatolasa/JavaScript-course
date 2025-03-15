daea045e93718617d5c89ef5498d08a1c4faf219
BonaTolasa <146835715+bonatolasa@users.noreply.github.com>
2025-03-15 10:21:58 +0300

    Arguments works value vs refrence commit

--- 08-Function/script.js
+++ 08-Function/script.js
@@ -1,4 +1,9 @@
 'use strict';
+
+/*
+////////////////////
+// DEFAULT PARAMETERS
+
 const bookings = [];
 
 const createBooking = function (
@@ -10,6 +15,7 @@ const createBooking = function (
   // numPassengers=numPassengers||1;
   // price=price||199;
 
+  //object"booking" store in bookings
   const booking = {
     flightNum,
     numPassengers,
@@ -26,3 +32,39 @@ createBooking('LE32', 5);
 createBooking('LE32', undefined, 222);
 
 console.log(bookings);
+*/
+
+///////////////////////
+// Arguments works value vs refrence
+const flight = '  ET234';
+const bonah = {
+  name: 'Bona Tolasa',
+  passport: 234574876787,
+};
+
+const ckeckIn = function (flightNum, passenger) {
+  flightNum = 'ET567';
+  passenger.name = 'Mr.' + passenger.name;
+  //   console.log(flightNum, passenger.name);
+
+  if (passenger.passport === 234574876787) {
+    alert('Checked In');
+  } else {
+    alert('Worng Passport');
+  }
+};
+
+ckeckIn(flight, bonah);
+console.log(flight, bonah);
+
+// The same as doing.....
+// const flightNum = flight;
+// const passenger = bonah;
+// console.log(flightNum, passenger);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+newPassport(bonah);
+console.log(flight, bonah);
+ckeckIn(flight, bonah);
