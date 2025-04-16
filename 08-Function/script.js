f7da71cba7169c1d0c59c5c10846b72699c84521
BonaTolasa <tolboni614@gmail.com>
2025-04-16 08:46:18 +0300

    first commit

--- 08-Function/script.js
+++ 08-Function/script.js
@@ -241,6 +241,7 @@ runOnce();
 console.log(offPrivate);
 */
 
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -257,3 +258,44 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+//EXAMPLE 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 3);
+  };
+};
+
+const h = function () {
+  const b = 435;
+  f = function () {
+    console.log(b * 5);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+//Re-Assigning
+h();
+f();
+console.dir(f);
+
+//Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+// const perGroup = 1000;
+boardPassengers(180, 3);
