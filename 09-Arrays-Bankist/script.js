567a60cd3f875a0764d58b4540df3450af019e1d
BonaTolasa <tolboni614@gmail.com>
2025-06-26 11:28:57 +0300

    computing usernames

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -78,6 +78,18 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsername(accounts);
+console.log(accounts);
 /////////////////////////////////////////////////
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
