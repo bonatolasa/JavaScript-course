a7a8b3e3508e4e2d9c503083cd5a45c84673e2e4
BonaTolasa <tolboni614@gmail.com>
2025-06-26 12:08:13 +0300

    reduce method used for calc balance

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -79,6 +79,12 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = balance;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsername = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
