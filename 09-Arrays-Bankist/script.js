e9c79973b66dd8843c69541f476dcb653671cd51
BonaTolasa <tolboni614@gmail.com>
2025-06-26 13:15:22 +0300

    interest sum of account commit

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -95,6 +95,16 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)} Birr`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest} Birr`;
 };
 calcDisplaySummary(account1.movements);
 
