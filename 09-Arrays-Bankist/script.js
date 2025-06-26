6aa8f066dcc24177b2a1d0e32793942877896fbd
BonaTolasa <tolboni614@gmail.com>
2025-06-26 18:12:19 +0300

    some method in loan money commit

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -173,6 +173,19 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //add movement
+    currentAccount.movements.push(amount);
+    //update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
