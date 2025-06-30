e8db520ef7e278bc80e59ce1d300957addedd7ca
BonaTolasa <tolboni614@gmail.com>
2025-06-30 15:33:06 +0300

    setTime in loan bankist

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -298,12 +298,14 @@ btnLoan.addEventListener('click', function (e) {
 
   const amount = Math.floor(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    //add movement
-    currentAccount.movements.push(amount);
-    //add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
-    //update UI
-    updateUI(currentAccount);
+    setTimeout(function () {
+      //add movement
+      currentAccount.movements.push(amount);
+      //add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
+      //update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
