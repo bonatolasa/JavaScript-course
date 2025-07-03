a308b8a44e39b8404f959d3a9c3635cadde08357
BonaTolasa <tolboni614@gmail.com>
2025-07-03 16:03:03 +0300

    constructor and prototype

--- 10-OOP/script.js
+++ 10-OOP/script.js
@@ -14,7 +14,7 @@ const Person = function (firstName, birthYear) {
 };
 
 const jonas = new Person('Jonas', 1991);
-// console.log(jonas);
+console.log(jonas);
 
 // 1. New {} is created
 // 2. function is called, this = {}
@@ -24,54 +24,54 @@ const jonas = new Person('Jonas', 1991);
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 
-// console.log(jonas instanceof Person);
+console.log(jonas instanceof Person);
 
 Person.hey = function () {
-  //   console.log('Hey there 👋');
-  //   console.log(this);
+  console.log('Hey there 👋');
+  console.log(this);
 };
 Person.hey();
 
 ///////////////////////////////////////
 // Prototypes
-// console.log(Person.prototype);
+console.log(Person.prototype);
 
 Person.prototype.calcAge = function () {
-  //   console.log(2037 - this.birthYear);
+  console.log(2037 - this.birthYear);
 };
 
 jonas.calcAge();
 matilda.calcAge();
 
-// console.log(jonas.__proto__);
-// console.log(jonas.__proto__ === Person.prototype);
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
 
-// console.log(Person.prototype.isPrototypeOf(jonas));
-// console.log(Person.prototype.isPrototypeOf(matilda));
-// console.log(Person.prototype.isPrototypeOf(Person));
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
 
 // .prototyeOfLinkedObjects
 
 Person.prototype.species = 'Homo Sapiens';
-// console.log(jonas.species, matilda.species);
+console.log(jonas.species, matilda.species);
 
-// console.log(jonas.hasOwnProperty('firstName'));
-// console.log(jonas.hasOwnProperty('species'));
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
 
 ///////////////////////////////////////
 // Prototypal Inheritance on Built-In Objects
-// console.log(jonas.__proto__);
-// // Object.prototype (top of prototype chain)
-// console.log(jonas.__proto__.__proto__);
-// console.log(jonas.__proto__.__proto__.__proto__);
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
 
-// console.dir(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
 
 const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
-// console.log(arr.__proto__);
-// console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
 
-// console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__.__proto__);
 
 Array.prototype.unique = function () {
   return [...new Set(this)];
@@ -80,7 +80,7 @@ Array.prototype.unique = function () {
 console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
-// console.dir(x => x + 1);
+console.dir(x => x + 1);
 
 ///////////////////////////////////////
 // Coding Challenge #1
