cb9264d287dd69d20ccde8c3ebd549cfd1cfc8ff
BonaTolasa <tolboni614@gmail.com>
2025-07-03 21:34:32 +0300

    Setters and Getters

--- 10-OOP/script.js
+++ 10-OOP/script.js
@@ -182,3 +182,23 @@ jessica.greet();
 
 const walter = new PersonCl('Walter White', 1965);
 // PersonCl.hey();
+
+///////////////////////////////////////
+// Setters and Getters
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
