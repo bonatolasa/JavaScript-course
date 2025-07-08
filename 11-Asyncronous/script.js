cc3cfd5f63cd7a58c8a6fcaf842c646253b81157
BonaTolasa <tolboni614@gmail.com>
2025-07-08 15:03:15 +0300

    Running Promises in Parallel

--- 11-Asyncronous/script.js
+++ 11-Asyncronous/script.js
@@ -223,6 +223,7 @@ const whereAmI = async function () {
 btn.addEventListener('click', whereAmI);
  */
 
+/*
 ///////////////////////////////////////////
 // Returning Values from Async Functions
 const getPosition = function () {
@@ -276,3 +277,31 @@ const whereAmI = async function () {
   }
   console.log('3: Finished getting location');
 })();
+*/
+
+///////////////////////////////////////
+// Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.com/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.com/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.com/v2/name/${c3}`
+    // );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('ethiopia', 'canada', 'portugal');
